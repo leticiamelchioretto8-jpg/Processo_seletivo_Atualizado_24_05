6cb7b42518a4764db2ca292e9619cb1cfb4eafc8
Leticia Melchioretto <leticiamelchioretto8@gmail.com>
2026-05-24 19:04:16 -0300

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,27 @@
 package frc.robot;
 
+//bibliotecas da REV  pros motores
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkRelativeEncoder;
 
+//Classe para executar o ciclo do codigo a cada 20 milissegundos e para mapear os botões do controle
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 
+//variaveis ou objetos que eu vou usar durante o resto do codigo
 public class Robot extends TimedRobot {
     private SparkMax frontLeft, backLeft, frontRight, backRight;
     private SparkMax shutterSuperior, shutterInferior;
     private SparkRelativeEncoder encoderEsquerdo, encoderDireito;
     private XboxController controle;
     private int contadorAutonomo;
-
+//constante do PD e a velocidade padrão no autonomo(que estou desenvolvendo).
     private static final double Kp = 0.03;
     private static final double Kd = 0.01;
     private static final double VELOCIDADE_BASE = 0.3;
 
-// Variáveis de estado do PD
+//erro de rota do ciclo anterior pro KD
 private double erroAnterior = 0;
     @Override
     public void robotInit() {
@@ -33,10 +36,11 @@ private double erroAnterior = 0;
 
         controle = new XboxController(0);
 
+        //comando para ler as rotações das rodas
         encoderEsquerdo = (SparkRelativeEncoder) frontLeft.getEncoder();
         encoderDireito = (SparkRelativeEncoder) frontRight.getEncoder();
     }
-
+//zera o cronometro e reseta as posições
     @Override
     public void autonomousInit() {
         contadorAutonomo = 0;
@@ -47,7 +51,7 @@ private double erroAnterior = 0;
     @Override
     public void autonomousPeriodic() {
       
-        if (contadorAutonomo < 100) {
+        if (contadorAutonomo < 100) { //cem ciclos para andar 2 segundos
             andarRetoComPD(VELOCIDADE_BASE);
         } else if (contadorAutonomo < 200) {
             // Para e aciona o shutter
@@ -57,36 +61,37 @@ private double erroAnterior = 0;
         } else {
             stopAll();
         }
-        contadorAutonomo++;
+        contadorAutonomo++; //adiciona 1 a cada fim de ciclo
     }
     private void andarRetoComPD(double velocidadeBase) {
-        // Erro = é positivo se o esquerdo andou mais
+        // Erro é positivo se o esquerdo andou mais e negativo se o direito andou mais
         double posEsquerda = encoderEsquerdo.getPosition();
         double posDireita  = encoderDireito.getPosition();
         double erro = posEsquerda - posDireita;
  
-        // Derivativa = variação do erro em relação ao ciclo anterior
+        //calcula a velocidade com que o robô está se desviando da linha reta e atualiza a variavel pro proximo ciclo que vai acontecer
         double derivativa = erro - erroAnterior;
         erroAnterior = erro;
  
         // Correção PD
         double correcao = (Kp * erro) + (Kd * derivativa);
  
-        // Aplica a correção: se o lado esquerdo avançou mais, reduz esquerda e aumenta direita
+        // Aplica a correção, se o lado esquerdo avançou mais, reduz esquerda e aumenta direita
         double vEsquerda =  velocidadeBase - correcao;
         double vDireita  = -(velocidadeBase + correcao); // negativo por inversão do lado direito
- 
+ //envia essas velocidades que foram calculadas para o motor
         acionarMotores(vEsquerda, vDireita);
     }
     @Override
     public void teleopPeriodic() {
-        double velocidade = -controle.getLeftY(); 
-        double rotacao = controle.getRightX();
+        double velocidade = -controle.getLeftY();  //negativo porque a WPI le o valor negativo quando o joystick vai para frente
+        double rotacao = controle.getRightX(); //curvas
 
-        // Cálculo de movimentação
+        // Calculo de movimentação, se quero somente ir para frente, fazer uma curva ou os dois ao mesmo tempo
         double esquerdo = velocidade + rotacao;
         double direito = -(velocidade - rotacao);
 
+        //envia os comandos do controle para as rodas.
         acionarMotores(esquerdo, direito);
         
         // Controle do Shutter
@@ -99,6 +104,7 @@ private double erroAnterior = 0;
         }
     }
 
+    //função para andar
     private void acionarMotores(double vEsquerda, double vDireita) {
         frontLeft.set(vEsquerda);
         backLeft.set(vEsquerda); // Segue 
@@ -106,7 +112,7 @@ private double erroAnterior = 0;
         frontRight.set(vDireita);
         backRight.set(vDireita); // Segue a direita
     }
-
+//função para quando largar os botões parar tudo
     private void stopAll() {
         acionarMotores(0, 0);
         shutterSuperior.set(0);
